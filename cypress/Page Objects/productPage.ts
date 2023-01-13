223476945663eb2d0858f0c6775ed97a3b7d4b5c
Ewelina Cieśla <hubert.mrozinski@gmail.com>
2023-01-13 20:11:30 +0100

    Creating method for mapping product names on product page

--- cypress/Page Objects/productPage.ts
+++ cypress/Page Objects/productPage.ts
@@ -4,10 +4,25 @@ class productPage {
     public shoppingCart = '.shopping_cart_container';
     public menuButton = '.bm-burger-button';
     public logoutButton = '#logout_sidebar_link';
-    public productNameArray : string[] = ['Sauce Labs Backpack', 'Sauce Labs Bike Light', 'Sauce Labs Bolt T-Shirt','Sauce Labs Fleece Jacket','Sauce Labs Onesie','Test.allTheThings() T-Shirt (Red)'];
+   // public productNameArray : string[] = ['Sauce Labs Backpack', 'Sauce Labs Bike Light', 'Sauce Labs Bolt T-Shirt','Sauce Labs Fleece Jacket','Sauce Labs Onesie','Test.allTheThings() T-Shirt (Red)'];
+   public productNameArray : string[] = []
 
+   mapProductNames() {
+    cy.get('.inventory_item_name').then(($products) => {
+        this.productNameArray = Array.from($products).map((el) => el.innerText);
+    });
+
+}
+
+    
     addToCart(productName: string) {
         cy.get(this.products).find(this.addToCartButton).eq(this.productNameArray.indexOf(productName)).click()
+    }
+
+    addEverythingToCart(){
+        for(let i=0; i< this.productNameArray.length; i++ ){
+            this.addToCart(this.productNameArray[i])
+        }
     }
 
     openShoppingCart() {
